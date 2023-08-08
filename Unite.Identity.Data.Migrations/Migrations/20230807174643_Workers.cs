818a073810361cfea3826736e3297e99d22a97b0
vulasau <valiantsin.ulasau@gmail.com>
2023-08-08 13:03:41 +0200

    Workers migration.

---
+++ Unite.Identity.Data.Migrations/Migrations/20230807174643_Workers.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace Unite.Identity.Data.Migrations.Migrations
+{
+    public partial class Workers : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Workers",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    Description = table.Column<string>(type: "text", nullable: true),
+                    Token = table.Column<string>(type: "text", nullable: true),
+                    TokenExpiryDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Workers", x => x.Id);
+                    table.UniqueConstraint("AK_Workers_Name", x => x.Name);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "WorkerPermissions",
+                columns: table => new
+                {
+                    WorkerId = table.Column<int>(type: "integer", nullable: false),
+                    PermissionId = table.Column<int>(type: "integer", nullable: false),
+                    Restrictions = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_WorkerPermissions", x => new { x.WorkerId, x.PermissionId });
+                    table.ForeignKey(
+                        name: "FK_WorkerPermissions_Permissions_PermissionId",
+                        column: x => x.PermissionId,
+                        principalTable: "Permissions",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_WorkerPermissions_Workers_WorkerId",
+                        column: x => x.WorkerId,
+                        principalTable: "Workers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_WorkerPermissions_PermissionId",
+                table: "WorkerPermissions",
+                column: "PermissionId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "WorkerPermissions");
+
+            migrationBuilder.DropTable(
+                name: "Workers");
+        }
+    }
+}
